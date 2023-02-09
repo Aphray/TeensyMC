dc12eaede2a72d77ba279b73fd922e4723d0c329
bkingsl1 <brycejkingsley@gmail.com>
2023-02-09 16:57:29 -0500

    Update TeensyMC.h

--- TeensyMC/TeensyMC.h
+++ TeensyMC/TeensyMC.h
@@ -1,2 +1,14 @@
 #pragma once
 
+#include "src/stepper.h"
+#include "src/serial_comm.h"
+#include "src/accelerator/accelerator.h"
+
+
+#ifndef REPORT_MILLIS_IDLE
+    #define REPORT_MILLIS_IDLE 500
+#endif
+
+#ifndef REPORT_MILLIS_ACTIVE
+    #define REPORT_MILLIS_ACTIVE 50
+#endif
