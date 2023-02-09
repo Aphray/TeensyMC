fac47c4909966cc0dafe7fea841bcbe39c31479c
Bryce Kingsley <brycejkingsley@gmail.com>
2023-02-09 00:03:30 -0500

    Update accelerator.h

--- include/accelerator.h
+++ include/accelerator.h
@@ -3,15 +3,6 @@
 #include <Arduino.h>
 #include "stepper.h"
 
-// float get_accelerator_speed();
-
-// uint32_t get_accelerator_step();
-
-// void prepare_accelerator(uint32_t steps, float initial_speed, float target_speed, float accel);
-
-// float compute_next_step_period();
-
-
 namespace Accelerator {
 
     extern float current_speed;
