162e830d81a88ac20f1c1dcf7e0754869f52cbd0
bkingsl1 <brycejkingsley@gmail.com>
2023-02-24 13:46:29 -0500

    Update TeensyMC.h

--- TeensyMC/TeensyMC.h
+++ TeensyMC/TeensyMC.h
@@ -5,7 +5,6 @@
 #include "src/communication/serial_command.h"
 #include "src/communication/message_agent.h"
 #include "src/stepper_control/stepper_control.h"
-#include "src/stepper_control/callbacks.h"
 
 
 void TMC_Begin();
