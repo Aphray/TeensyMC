c983153bd34b406aea2ff7abe9557033724cc0b7
Bryce Kingsley <brycejkingsley@gmail.com>
2023-03-01 16:23:43 -0500

    Update TeensyMC.h

--- src/TeensyMC.h
+++ src/TeensyMC.h
@@ -1,11 +1,11 @@
 #pragma once
 
 #include <Arduino.h>
-#include "src/stepper_control/stepper.h"
-#include "src/communication/serial_command.h"
-#include "src/communication/message_agent.h"
-#include "src/stepper_control/stepper_control.h"
-#include "src/event_handling/event_manager.h"
+#include "stepper_control/stepper.h"
+#include "communication/serial_command.h"
+#include "communication/message_agent.h"
+#include "stepper_control/stepper_control.h"
+#include "event_handling/event_manager.h"
 
 
 void TMC_Begin();
