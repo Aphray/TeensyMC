293b85acb2036eb1c491e8369012c1269223ff5a
Bryce Kingsley <brycejkingsley@gmail.com>
2023-05-26 23:28:01 -0400

    split ArgList in separate header

---
+++ src/communication/arg_list.cpp
@@ -0,0 +1,49 @@
+#include "arg_list.h"
+
+ArgList::ArgList(char* args_) {
+    num_args = 0;
+    arg_idx = 0;
+
+    if (args_ != nullptr) {
+        split_args(args_);
+    }
+}
+
+void ArgList::split_args(char* args_) {
+    char* ptr = args[0];
+
+    do {
+        switch (*args_) {
+            case 0:
+                *ptr = 0;
+                if (args[num_args][0] != 0) { num_args++; }
+                break;
+            case ARG_DELIMITER_CHAR:
+                *ptr = 0;
+                ptr = args[(args[num_args][0] != 0 ? ++num_args : num_args)];
+                break;
+            default:
+                *ptr++ = *args_;
+                break;
+        }
+    } while (*args_++ && num_args < CMD_MAX_ARGS);
+}
+
+char* ArgList::next() {
+    return (arg_idx < num_args) ? args[arg_idx++] : nullptr;
+}
+
+void ArgList::reset() {
+    arg_idx = 0;
+}
+
+uint8_t ArgList::get_num_args() {
+    return num_args;
+}
+
+void ArgList::copy(ArgList* arg_list) {
+    num_args = arg_list->num_args;
+    for (uint8_t n = 0; n < num_args; n++) {
+        strcpy(args[n], arg_list->args[n]);
+    }
+}
---
+++ src/communication/arg_list.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <Arduino.h>
+#include "../config.h"
+
+
+class ArgList {
+    public:
+        ArgList() {};
+        ArgList(char* args);
+
+        // get the next argument in the list
+        char* next();
+
+        // reset the indexer
+        void reset();
+
+        // returns the number of arguments 
+        uint8_t get_num_args();
+
+        // copy from another argument list
+        void copy(ArgList* arg_list);
+    
+    private:
+        uint8_t arg_idx;
+        uint8_t num_args;
+
+        char args[CMD_MAX_ARGS][ARG_BUFFER_SIZE];
+
+        // split the args c-string into 
+        void split_args(char* args);
+};
